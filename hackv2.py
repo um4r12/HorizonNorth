f7fe9e71cf69ad9d85fdbbe1f5784580cfdb2d2e
Muhammad Umer Khan <um4r12@gmail.com>
2019-01-13 11:56:53 -0700

    initializing repo

---
+++ hackv2.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import json
+
+genreData = pd.read_csv('data/movies_metadata2.csv', usecols = ['genres'])
+data = pd.read_csv('data/movies_metadata.csv', usecols = ['title','genres','vote_average','release_date','id'])
+
+listgenre = []
+for key, value in genreData.iteritems():
+    for val in value:
+        val = json.loads(val)
+        if (len(val) > 0):
+            genre = (val[0]['name'])
+            listgenre.append(genre)
+        else:
+            listgenre.append('NaN')
+
+df = pd.DataFrame(listgenre)
+data['genres'] = df
+data = data[data['genres'] != 'NaN']
+data = data[data['genres'] != 'Odyssey Media']
+data = data[data['genres'] != 'Aniplex']
+data = data[data['genres'] != 'Carousel Productions']
+
+# change object into datetime then sort by release date descending old -> new
+data.dropna(how='any', inplace=True)
+data['release_date'] =  data['release_date'].astype('datetime64[ns]')
+data = data[(data['release_date'].dt.year >= 1950) & (data['release_date'].dt.year != 2018) & (data['release_date'].dt.year != 2020)]
+
+data.sort_values(by=['release_date'], inplace=True, ascending=True)
+
+# change vote average from object to int then sort values
+data['vote_average'] = data['vote_average'].fillna(0).astype(np.int)
+data.sort_values(by=['vote_average'], inplace=True, ascending=True)
+
+
+def genList(genre):
+    amountperGenre = []
+
+    for i in range(1950,2018):
+        temp = data.loc[(data['release_date'].dt.year == i) & (data['genres'] == genre)] #'Action'
+        amountperGenre.append(temp.shape[0])
+    return amountperGenre
+
+if __name__ == '__main__':
+
+    genList(genreType)
---
+++ myapp.py
@@ -0,0 +1,81 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
+import numpy as np
+import hackv2 as dp
+
+def getDropDownValues():
+    return [{'label': 'Action', 'value': 'Action'},
+{'label': 'Adventure', 'value': 'Adventure'},
+{'label': 'Animation', 'value': 'Animation'},
+{'label': 'Comedy', 'value': 'Comedy'},
+{'label': 'Crime', 'value': 'Crime'},
+{'label': 'Documentary', 'value': 'Documentary'},
+{'label': 'Drama', 'value': 'Drama'},
+{'label': 'Family', 'value': 'Family'},
+{'label': 'Fantasy', 'value': 'Fantasy'},
+{'label': 'Foreign', 'value': 'Foreign'},
+{'label': 'History', 'value': 'History'},
+{'label': 'Horror', 'value': 'Horror'},
+{'label': 'Music', 'value': 'Music'},
+{'label': 'Mystery', 'value': 'Mystery'},
+{'label': 'Romance', 'value': 'Romance'},
+{'label': 'Science Fiction' , 'value': 'Science Fiction' },
+{'label': 'TV' , 'value': 'Tv' },
+{'label': 'Thriller', 'value': 'Thriller'},
+{'label': 'War', 'value': 'War'},
+{'label': 'Western', 'value': 'Western'}]
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+
+yearBeginnning = 1970
+years = list(range(yearBeginnning, yearBeginnning+50))
+numMales= np.random.randint(low=0, high=20, size=50)
+numFemales= np.random.randint(low=0, high=20, size=50)
+
+
+
+
+app.layout = html.Div(children=[         
+    html.H1('Horizon North'),
+    html.Div([
+    html.P('Genre trends across time'),
+    html.P('This conversion happens behind the scenes by Dash JavaScript front-end')
+    ]),    
+    dcc.Graph(id='freq-graph'),
+    dcc.Dropdown(
+        id='genre-type',
+        options=getDropDownValues(),
+        value='action'
+    )        
+])
+
+
+@app.callback(
+    dash.dependencies.Output('freq-graph', 'figure'),
+    [dash.dependencies.Input('genre-type', 'value')])
+
+def update_graph(genreType):
+   
+    count = dp.genList(genreType.title())
+    return {
+            'data': [go.Scatter(
+                x = [i for i in range(1950,2018)],
+                y = count,
+                mode = 'lines+markers',
+                name = 'Count'
+            )               
+                
+            ]
+        }    
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
